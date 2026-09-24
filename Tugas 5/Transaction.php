a78fbbd96baf3e76bbe4e071cc774ec7b46da98d
unknown <heniel635@gmail.com>
2026-09-24 12:16:34 +0800

    Menambahkan method process dengan match expression untuk jenis transaksi

--- Tugas 5/Transaction.php
+++ Tugas 5/Transaction.php
@@ -24,4 +24,40 @@ class Transaction
     {
         return $this->amount;
     }
+
+    public function process(): bool|string
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        // Inisialisasi saldo di sesi jika belum ada
+        if (!isset($_SESSION['balance'])) {
+            $_SESSION['balance'] = 0.0;
+        }
+        if (!isset($_SESSION['transactions'])) {
+            $_SESSION['transactions'] = [];
+        }
+
+        // Ekspresi match untuk mencocokkan jenis transaksi
+        $processResult = match ($this->type) {
+            'deposit' => $this->handleDeposit(),
+            'withdraw' => $this->handleWithdraw(),
+            default => 'Jenis transaksi tidak valid.'
+        };
+
+        return $processResult;
+    }
+
+    private function handleDeposit(): bool
+    {
+        // TODO: implementasi logika deposit
+        return true;
+    }
+
+    private function handleWithdraw(): bool|string
+    {
+        // TODO: implementasi logika penarikan dengan validasi saldo
+        return true;
+    }
 }
